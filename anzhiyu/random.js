7159d29a1d8a17b8266877d170f5c36939a114bc
heng6864 <heng686462@gmail.com>
2025-02-25 22:06:21 +0800

    Site updated: 2025-02-25 22:06:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/25/hello-world/","2025/02/25/这是另一篇新的博文/"];function toRandomPost(){
+var posts=["2025/02/25/这是另一篇新的博文/","2025/02/25/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
